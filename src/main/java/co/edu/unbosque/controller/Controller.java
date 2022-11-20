e93c1334ec7bdcc2f47e307eb4526619193bc27e
Lau-lalo11 <lamateus@unbosque.edu.co>
2022-11-19 19:36:05 -0500

    inter

--- src/main/java/co/edu/unbosque/controller/Controller.java
+++ src/main/java/co/edu/unbosque/controller/Controller.java
@@ -12,35 +12,10 @@ public class Controller {
     }
 
     public static void main(String[] args) {
+        
         PanelPrincipal panel= new PanelPrincipal();
         panel.mostrarPanelResultado();
-        double[][] a = {{4., 2.}, {1., 3.}};
-        double[][] b = {{2., 8.}, {3., 1.}};
-        Matrix matris = new Matrix(a);
-        Matrix matris2 = new Matrix(b);
-        System.out.println(matris.det() + "-----");
-//        double[][] ete=matris.getArray();
-//        for(int x=0;x<2;x++){
-//            for(int y=0;y<2;y++){
-//                System.out.println(ete[x][y]);
-//            }
-//        }
 
-
-
-        double[][] ete2 = matris.inverse().getArray();
-        for (int x = 0; x < 2; x++) {
-            for (int y = 0; y < 2; y++) {
-                System.out.println(ete2[x][y]);
-            }
-        }
-
-//        double[][] ete2=matris.times(matris2).getArray();
-//        for(int x=0;x<2;x++){
-//            for(int y=0;y<2;y++){
-//                System.out.println(ete2[x][y]);
-//            }
-//        }
     }
 
 
